0cda8ef20c76b7dd9c963a84a5aa189b1214f225
Tameem <tameem_r@hotmail.com>
2021-03-15 21:37:35 +0200

    Basic html & css, and fetching API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "weather-icons/css/weather-icons.min.css";
+import Weather from "./app_component/weather.component";
+
+//api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
+const API_key = "1952cc01bff81ba8bf8650114746df46";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.getWeather();
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`
+    );
+
+    const response = await api_call.json();
+
+    console.log(response);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
